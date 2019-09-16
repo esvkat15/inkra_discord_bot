e06e0f252303b2c58799f371014aa7dec7e2a543
esvkat15 <matthewhmunoz@gmail.com>
2019-09-16 10:36:03 -0700

    publishing for discord channel

--- src-electron/App.vue
+++ src-electron/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    -->
+
+    <!-- lets start here...
+      todo:
+
+      decide on a css framework for material components
+      decide wether we need babel
+      start working on the UI design
+
+      later:
+
+      integrate with discordjs to actually respond to commands
+      integrate interprocess communication with mono fsharp process for handling data
+      start writing said process
+    -->
+
   </div>
 </template>
 
